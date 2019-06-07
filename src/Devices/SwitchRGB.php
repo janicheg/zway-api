38f0ea0ba9893223ac81003883c9b646fd64b9a5
janicheg <jan@kurbanov.me>
2019-06-07 18:33:31 +0400

    fix

--- src/Devices/SwitchRGB.php
+++ src/Devices/SwitchRGB.php
@@ -8,7 +8,7 @@ class SwitchRGB extends SwitchBinary
 {
     const TYPE_NAME = 'switchRGB';
 
-    public function setTemperature(int $red, int $green, int $blue)
+    public function setRGB(int $red, int $green, int $blue)
     {
         $command = new ExactRGBCommand($this->id, $this->api);
         return $command->set($red, $green, $blue);
