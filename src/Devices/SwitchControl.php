8fc5e519788a0909499af8817f85847d5223fe08
janicheg <jan@kurbanov.me>
2019-06-26 17:21:09 +0400

    fix

--- src/Devices/SwitchControl.php
+++ src/Devices/SwitchControl.php
@@ -2,15 +2,54 @@
 
 namespace ZWay\Devices;
 
-use ZWay\Commands\Device\OnCommand;
+use ZWay\Commands\Device\DownstartCommand;
+use ZWay\Commands\Device\DownstopCommand;
+use ZWay\Commands\Device\ExactCommand;
+use ZWay\Commands\Device\UpstartCommand;
+use ZWay\Commands\Device\UpstopCommand;
 
-class SwitchControl extends Thermostat
+class SwitchControl extends SwitchBinary
 {
     const TYPE_NAME = 'switchControl';
 
-    public function turnOn()
+    /**
+     * @param int $value
+     * @return \ZWay\Responses\Response
+     */
+    public function set(int $value)
     {
-        $command = new OnCommand($this->id, $this->api);
-        return $command->on();
+        return (new ExactCommand($this->id, $this->api))->set($value);
+    }
+
+    /**
+     * @return \ZWay\Responses\Response
+     */
+    public function downstart()
+    {
+        return (new DownstartCommand($this->id, $this->api))->downstart();
+    }
+
+    /**
+     * @return \ZWay\Responses\Response
+     */
+    public function downstop()
+    {
+        return (new DownstopCommand($this->id, $this->api))->downstop();
+    }
+
+    /**
+     * @return \ZWay\Responses\Response
+     */
+    public function upstart()
+    {
+        return (new UpstartCommand($this->id, $this->api))->upstart();
+    }
+
+    /**
+     * @return \ZWay\Responses\Response
+     */
+    public function upstop()
+    {
+        return (new UpstopCommand($this->id, $this->api))->upstop();
     }
 }
